a3fdc4c717cca2a291d07c574a6d7a7e3cef194c
Sebastian Flosbach <sebastian.flosbach@gmail.com>
2017-04-19 11:22:52 +0200

    Tree view expamle for showing item information

--- FactorioWpf/MainWindow.xaml.cs
+++ FactorioWpf/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using Factorio;
+using Factorio.Entities;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FactorioWpf
 {
@@ -20,16 +13,77 @@ namespace FactorioWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        IFactorioLogic logic;
         public MainWindow()
         {
             InitializeComponent();
 
+            logic = new FactorioLogic();
+
+            logic.ReadFile("ItemList.xml");
+
             TreeViewItem item = new TreeViewItem()
             {
-                Header = "MyHeader"
+                Header = "Items"
             };
 
             FolderView.Items.Add(item);
+
+            item.Expanded += TreeView_Expanded;
+
+            //Add dummy item
+            item.Items.Add(null);
+
+
+        }
+
+        private void TreeView_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem treeItem = sender as TreeViewItem;
+
+            if(treeItem.Items[0] == null)
+            {
+                treeItem.Items.RemoveAt(0);
+
+                foreach(var item in logic.Items)
+                {
+                    var newTreeItem = new TreeViewItem()
+                    {
+                        Header = item.Name
+                    };
+
+                    newTreeItem.Expanded += Item_Expanded;
+
+                    //Add dummy item
+                    newTreeItem.Items.Add(null);
+
+                    treeItem.Items.Add(newTreeItem);
+                }
+            }
+        }
+
+        private void Item_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = sender as TreeViewItem;
+
+            if(item.Items[0] == null)
+            {
+                item.Items.RemoveAt(0);
+
+                var tempItem = logic.Items.Find(x => x.Name == (string)item.Header);
+
+                item.Items.Add(new TextBlock()
+                {                    
+                    Text =  $"Output: {tempItem.CraftingOutput} items\n" +
+                            $"CraftTime: {tempItem.CraftingTime} seconds\n" +
+                            $"Productivity: {tempItem.Productivity} items per second\n" +
+                            (tempItem.Recipe == null ? "This item has no recipe" : "This item has a recipe"),
+                    Foreground = Brushes.Black,
+                    
+                    
+                }
+                    );
+            }
         }
     }
 }
