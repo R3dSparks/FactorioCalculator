02dff699bbc7a35273895b94e8f13eba5cc1bf39
Sebastian Flosbach <sebastian.flosbach@gmail.com>
2017-04-19 12:15:00 +0200

    Comments, small changes

--- FactorioWpf/MainWindow.xaml.cs
+++ FactorioWpf/MainWindow.xaml.cs
@@ -37,12 +37,18 @@ namespace FactorioWpf
 
         }
 
+        /// <summary>
+        /// Expand the main node to get FactorioItems
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TreeView_Expanded(object sender, RoutedEventArgs e)
         {
             TreeViewItem treeItem = sender as TreeViewItem;
 
             if(treeItem.Items[0] == null)
             {
+                //Remove dummy item
                 treeItem.Items.RemoveAt(0);
 
                 foreach(var item in logic.Items)
@@ -62,25 +68,29 @@ namespace FactorioWpf
             }
         }
 
+        /// <summary>
+        /// Expand an item to get the item properties
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Item_Expanded(object sender, RoutedEventArgs e)
         {
             var item = sender as TreeViewItem;
 
             if(item.Items[0] == null)
             {
+                //Remove dummy item
                 item.Items.RemoveAt(0);
 
                 var tempItem = logic.Items.Find(x => x.Name == (string)item.Header);
 
                 item.Items.Add(new TextBlock()
                 {                    
-                    Text =  $"Output: {tempItem.CraftingOutput} items\n" +
+                    Text =  $"Output: {tempItem.CraftingOutput} item" + (tempItem.CraftingOutput == 1 ? "" : "s") + "\n" +
                             $"CraftTime: {tempItem.CraftingTime} seconds\n" +
                             $"Productivity: {tempItem.Productivity} items per second\n" +
                             (tempItem.Recipe == null ? "This item has no recipe" : "This item has a recipe"),
-                    Foreground = Brushes.Black,
-                    
-                    
+                    Foreground = Brushes.Black,                    
                 }
                     );
             }
