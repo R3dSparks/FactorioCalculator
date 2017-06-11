1a79d1bdc4087b59bfd96206e32d5a3a0e4a57af
Nico Vogel <nico-vogel97@outlook.de>
2017-06-11 13:04:11 +0200

    - added settings class for the production view - added image class which will represent an assembly (not all needed properties are created yet)

---
+++ FactorioWpf/Helper/ProductionViewer/ProductionViewImage.cs
@@ -0,0 +1,78 @@
+﻿using Factorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioWpf.Helper
+{
+    public class ProductionViewImage
+    {
+
+        #region Private variables
+
+
+        FactorioAssembly m_relatedAssembly;
+        ProductionViewSettings m_settings;
+
+
+        #endregion
+
+        #region Properties
+
+
+        /// <summary>
+        /// Reference to the Assembly which is represented by this class
+        /// </summary>
+        public FactorioAssembly PatrentAssembly
+        {
+            get { return m_relatedAssembly; }
+            private set { m_relatedAssembly = value; }
+        }
+
+        /// <summary>
+        /// Reference to the settings class
+        /// </summary>
+        public ProductionViewSettings ViewSettings {
+            get { return m_settings; }
+            private set { m_settings = value; }
+        }
+
+        /// <summary>
+        /// The amount of images which are located left to the current image to calculate the spaceing. 
+        /// It does not represent the total value of images located left.
+        /// </summary>
+        public int ImageLeftCount { get; set; }
+
+        /// <summary>
+        /// The amount of sub images below the current image.
+        /// </summary>
+        public int SubImageWith { get; set; }
+
+
+        #endregion
+
+        #region Constructions
+
+
+        /// <summary>
+        /// default consturctior
+        /// </summary>
+        /// <param name="assambly">this is the parrent assembly</param>
+        /// <param name="setting">view settings class reference</param>
+        public ProductionViewImage(FactorioAssembly assambly, ProductionViewSettings setting)
+        {
+            this.PatrentAssembly = assambly;
+            this.ViewSettings = setting;
+        }
+
+
+
+        #endregion
+
+
+
+
+    }
+}
---
+++ FactorioWpf/Helper/ProductionViewer/ProductionViewSettings.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioWpf.Helper
+{
+    /// <summary>
+    /// This class contain values which are used in calculating the view for the production viewer
+    /// </summary>
+    public class ProductionViewSettings
+    {
+
+        #region Default Values
+
+
+        private int m_imageWidth = 30;
+        private int m_imageHeight = 30;
+        private int m_heightOffset = 40;
+        private int m_widthOffset = 20;
+
+
+        #endregion
+        
+        #region Properties
+
+
+
+        /// <summary>
+        /// The width of all images in the production view
+        /// </summary>
+        public int ImageWidth { get; set; }
+
+        /// <summary>
+        /// The hight of all images in the production view
+        /// </summary>
+        public int ImageHeight { get; set; }
+
+        /// <summary>
+        /// Space in the height between pictures
+        /// </summary>
+        public int HeightOffset { get; set; }
+
+        /// <summary>
+        /// space in the with between pirctures
+        /// </summary>
+        public int WidthOffset { get; set; }
+
+
+
+        #endregion
+
+        #region Consturctor
+
+
+        /// <summary>
+        /// default constructor. It uses the default values
+        /// </summary>
+        public ProductionViewSettings()
+        {
+            this.ImageHeight = m_imageHeight;
+            this.ImageWidth = m_imageWidth;
+            this.HeightOffset = m_heightOffset;
+            this.WidthOffset = m_widthOffset;
+        }
+
+        /// <summary>
+        /// constructor with defined values for all settings
+        /// </summary>
+        /// <param name="imageHight"></param>
+        /// <param name="imageWidth"></param>
+        /// <param name="heightOffset"></param>
+        /// <param name="widthOffset"></param>
+        public ProductionViewSettings(int imageHight, int imageWidth, int heightOffset, int widthOffset)
+        {
+            this.ImageHeight = imageHight;
+            this.ImageWidth = imageWidth;
+            this.HeightOffset = heightOffset;
+            this.WidthOffset = widthOffset;
+        }
+
+
+        #endregion
+        
+    }
+}
