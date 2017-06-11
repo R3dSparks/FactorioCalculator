59a90afe90571c65d82e682d95f1cc07d6e45574
Sebastian Flosbach <sebastian.flosbach@gmail.com>
2017-06-11 11:02:38 +0200

    - Added ItemData

--- FactorioWpf/ViewModels/Base/BaseViewModell.cs
+++ FactorioWpf/ViewModels/Base/BaseViewModell.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 
 namespace FactorioWpf.ViewModels
 {
-    [ImplementPropertyChanged]
     public abstract class BaseViewModell : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
